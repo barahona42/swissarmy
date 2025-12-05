9d95c25b7b33fcac57254ccd5d8de2c1cc55a25a
Rene Barahona <rene.barahona@pnmac.com>
2025-12-04 20:57:21 -0800

    adds serdecodecs

--- knifego/go.mod
+++ knifego/go.mod
@@ -1,3 +1,8 @@
 module github.com/barahona42/swissarmy/knifego
 
 go 1.25.5
+
+require (
+	go.mongodb.org/mongo-driver/v2 v2.4.0
+	google.golang.org/protobuf v1.36.10
+)
---
+++ knifego/serdecodecs/serdecodecs.go
@@ -0,0 +1,70 @@
+/*
+serdecodecs provides utilities for serialization and de-serialization of custom types.
+*/
+package serdecodecs
+
+import (
+	"fmt"
+	"reflect"
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+// TimestampCodec: implements bson.Value{Encoder,Decoder} interface for serde of `*timestamppb.Timestamp`.
+type TimestampCodec struct{}
+
+// DecodeValue: value encoder.
+func (codec TimestampCodec) EncodeValue(_ bson.EncodeContext, vw bson.ValueWriter, val reflect.Value) error {
+	if !val.IsValid() {
+		return bson.ValueEncoderError{
+			Name:     "TimestampEncodeValue",
+			Kinds:    []reflect.Kind{reflect.TypeOf(timestamppb.Timestamp{}).Kind()},
+			Received: val,
+		}
+	}
+	t, ok := val.Interface().(*timestamppb.Timestamp)
+	if !ok {
+		return bson.ValueEncoderError{
+			Name:     "TimestampEncodeType",
+			Kinds:    []reflect.Kind{reflect.TypeOf(timestamppb.Timestamp{}).Kind()},
+			Received: val,
+		}
+	}
+	// timestamppb.Timestamp starts at Unix start
+	return vw.WriteDateTime(t.AsTime().UnixMilli())
+}
+
+// DecodeValue: value decoder.
+func (codec TimestampCodec) DecodeValue(_ bson.DecodeContext, vr bson.ValueReader, val reflect.Value) error {
+	if !val.IsValid() {
+		return bson.ValueDecoderError{
+			Name:     "TimestampDecodeValue",
+			Kinds:    []reflect.Kind{reflect.Kind(bson.TypeDateTime)},
+			Received: val,
+		}
+	}
+	if vr.Type() != bson.TypeDateTime {
+		return bson.ValueDecoderError{
+			Name:     "TimestampDecodeType",
+			Kinds:    []reflect.Kind{reflect.Kind(bson.TypeDateTime)},
+			Received: val,
+		}
+	}
+	t, err := vr.ReadDateTime()
+	if err != nil {
+		return fmt.Errorf("read datetime: %w", err)
+	}
+	val.Set(reflect.ValueOf(timestamppb.New(time.UnixMilli(t))))
+	return nil
+}
+
+// NewRegistry: creates a new bson registry that adds codecs defined in this package.
+func NewRegistry() *bson.Registry {
+	r := bson.NewRegistry()                       // not actually empty as docstring says. includes "default" codecs
+	t := reflect.TypeOf(&timestamppb.Timestamp{}) // pointer is important to prevent copying the sync.Mutex value
+	r.RegisterTypeEncoder(t, TimestampCodec{})
+	r.RegisterTypeDecoder(t, TimestampCodec{})
+	return r
+}
